247c32fb45230848c4994a3cd1e91a842c8643b1
George Bratsos <geobratsos@gmail.com>
2019-09-04 14:08:45 +0300

    webpack update and package json updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,42 +1,68 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const autoprefixer = require('autoprefixer')
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'development',
-  devtool: "source-map",
+  devtool: 'source-map',
+  entry: './src/index.js',
   watch: true,
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: "style-loader"
-        },
-        {
-          loader: "css-loader", options: {
-            sourceMap: true
-          }
-        },
-        {
-          loader: "postcss-loader",
-          options: {
-            autoprefixer: {
-              browsers: ["last 2 versions"]
-            },
-            sourceMap: true,
-            plugins: () => [
-              autoprefixer
-            ]
-          },
-        },
-        {
-          loader: "sass-loader", options: {
-            sourceMap: true
-          }
-        }
-      ]
-    }],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
   },
-})
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ['last 2 versions', 'ie >= 9']
+              },
+              sourceMap: true,
+              plugins: () => [
+                autoprefixer
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '../../../../fonts/[name].[ext]'
+          }
+        }]
+      },
+    ],
+  },
+  plugins: [
+  ]
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,33 +1,59 @@
-const merge = require('webpack-merge');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const common = require('./webpack.common.js');
+const autoprefixer = require('autoprefixer');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'production',
+  entry: './src/index.js',
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: false // set to true if you want JS source maps
-      }),
       new OptimizeCSSAssetsPlugin({})
     ]
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        "css-loader", "sass-loader"
-      ]
-    }]
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: "postcss-loader",
+            options: {
+              autoprefixer: {
+                browsers: ['last 2 versions', 'ie >= 9']
+              },
+              sourceMap: false,
+              plugins: () => [
+                autoprefixer
+              ]
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|gif|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        }]
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "style.css",
+      filename: 'style.css'
     })
   ]
-})
+};
