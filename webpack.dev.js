7f1572a8457754da45be6a5915f36dfb375b78d3
George Bratsos <geobratsos@gmail.com>
2019-12-03 17:15:12 +0200

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,7 @@ module.exports = {
   watch: true,
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.bundle.js'
   },
   module: {
     rules: [
