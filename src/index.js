1d4bf67ae5c9e1ea5a483a6e120d2e4e9ee3838e
George Bratsos <geobratsos@gmail.com>
2020-01-31 14:47:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './scss/style.scss';
 
-console.log('Wedia is here');
+console.log('I am is here');
+
+//import '../node_modules/sharer.js/sharer.min';
+
+import './js/matchHeight';
+import './js/iframe';
+import './js/smoothscroll';
+import './js/captcha';
